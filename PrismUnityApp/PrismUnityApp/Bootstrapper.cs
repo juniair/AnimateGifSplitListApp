f0fe0d8d68e50c7bb062fa9a3c7f756660dc9c5a
juniair <juniair.kim@gmail.com>
2017-04-11 16:22:40 +0900

    Service를 Bootstrapper에 추가

--- PrismUnityApp/PrismUnityApp/Bootstrapper.cs
+++ PrismUnityApp/PrismUnityApp/Bootstrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Practices.Unity;
 using Prism.Modularity;
 using Prism.Unity;
+using PrismUnityApp.Service;
 using PrismUnityApp.Views;
 using System.Windows;
 
@@ -18,11 +19,14 @@ namespace PrismUnityApp
             Application.Current.MainWindow.Show();
         }
 
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            this.RegisterTypeIfMissing(typeof(IImageService), typeof(ImageService), true);
+        }
+
         protected override IModuleCatalog CreateModuleCatalog()
         {
-
-   
-
             return new DirectoryModuleCatalog() { ModulePath = @".\Modules" };
         }
     }
