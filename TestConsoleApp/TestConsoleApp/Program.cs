7c4400a88b1b60ad993c7252dd0b6cb5086042be
juniair <juniair.kim@gmail.com>
2017-05-10 10:05:54 +0900

    update

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using MetadataExtractor;
+using RavuAlHemio.PSD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Regex(@"(\.jpg|jpeg|gif|bmp|png|psd)", RegexOptions.IgnoreCase);
+            var s = r.Split("test.jpg.jpg");
+
+            foreach(string str in s)
+            {
+                Console.WriteLine(str);
+            }
+            
+
+
+        }
+    }
+}
